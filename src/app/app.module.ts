a45e1ec55020c1076c75d4188dbf8a988f071c30
AvelJonathan <jonathan.avel8@gmail.com>
2018-03-26 17:01:12 +0200

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { ScoreHistoryComponent } from './score-history/score-history.component';
+import { WallOfFameComponent } from './wall-of-fame/wall-of-fame.component';
+import { ChatComponent } from './chat/chat.component';
+import { ProfilComponent } from './profil/profil.component';
+import { GameComponent } from './game/game.component';
+import { TitleComponent } from './title/title.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    ScoreHistoryComponent,
+    WallOfFameComponent,
+    ChatComponent,
+    ProfilComponent,
+    GameComponent,
+    TitleComponent
   ],
   imports: [
     BrowserModule
