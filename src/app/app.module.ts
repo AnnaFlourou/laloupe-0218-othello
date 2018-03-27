dc373a334b3cb023f126beca0f7fb57c7d496084
Jo-zTk <jonathan.zietek@gmail.com>
2018-03-27 11:14:01 +0200

    Feat/Routing: Add routing & 404 Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, Component } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
+import { AppComponent } from "./app.component";
+import { AuthComponent } from "./auth/auth.component";
+import { ScoreHistoryComponent } from "./score-history/score-history.component";
+import { WallOfFameComponent } from "./wall-of-fame/wall-of-fame.component";
+import { ChatComponent } from "./chat/chat.component";
+import { ProfilComponent } from "./profil/profil.component";
+import { GameComponent } from "./game/game.component";
+import { TitleComponent } from "./title/title.component";
+import { FourOFourComponent } from "./four-o-four/four-o-four.component";
 
-import { AppComponent } from './app.component';
-import { AuthComponent } from './auth/auth.component';
-import { ScoreHistoryComponent } from './score-history/score-history.component';
-import { WallOfFameComponent } from './wall-of-fame/wall-of-fame.component';
-import { ChatComponent } from './chat/chat.component';
-import { ProfilComponent } from './profil/profil.component';
-import { GameComponent } from './game/game.component';
-import { TitleComponent } from './title/title.component';
-
+const appRoutes: Routes = [
+  { path: "login", component: AuthComponent },
+  { path: "game", component: GameComponent },
+  { path: "", component: AuthComponent },
+  { path: "404", component: FourOFourComponent },
+  { path: "**", redirectTo: "404" }
+];
 
 @NgModule({
   declarations: [
@@ -21,12 +29,11 @@ import { TitleComponent } from './title/title.component';
     ChatComponent,
     ProfilComponent,
     GameComponent,
-    TitleComponent
+    TitleComponent,
+    FourOFourComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
