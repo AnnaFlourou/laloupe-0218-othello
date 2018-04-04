d8a56f111ba07b8f150194ed5118e47bdd0bf7bc
AvelJonathan <jonathan.avel8@gmail.com>
2018-04-04 10:40:31 +0200

    add game table

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss']
+  styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
 
