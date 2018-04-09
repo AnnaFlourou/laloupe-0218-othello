4adbb9984d74f3edcab3bf724857db8f8f5d4526
Jo-zTk <jonathan.zietek@gmail.com>
2018-04-09 17:22:31 +0200

    Models: Add board creation on room model

--- src/app/profil/profil.component.ts
+++ src/app/profil/profil.component.ts
@@ -8,7 +8,7 @@ import { AuthService } from '../core/auth.service';
 })
 export class ProfilComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService) { }
 
   ngOnInit() {
   }
