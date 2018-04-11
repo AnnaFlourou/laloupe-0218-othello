9d55744a1dde5f30933d43a6bf63947be8b8da9a
AvelJonathan <jonathan.avel8@gmail.com>
2018-04-11 15:55:37 +0200

    Add 3 function game

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -46,9 +46,39 @@ export class BoardComponent implements OnInit {
   }
 
   click(x: number, y: number) {
-    this.room.board[x].line[y] = 1;
+    if (this.room.board[x].line[y] !== 0) { return;}
+    /* Distribute black and white pieces for players */
+    if (this.auth.myId === this.room.players[0].name) {
+      this.room.board[x].line[y] = 1;
+    } else {
+      this.room.board[x].line[y] = 2;
+    }
+    /* Count remaining pieces */
+    this.room.piece -= 1;
+    /* Turn System */
     this.room.turn = this.auth.myId;
     this.updateRoom();
+    if (this.room.piece === 0) {
+      let player1 = 0;
+      let player2 = 0;
+
+      for (let line = 0; line < 8; line += 1) {
+        for (let col = 0; col < 8; col += 1) {
+          if (this.room.board[line].line[col] === 1) {
+            player1 += 1;
+          } else {
+            player2 += 1 ;
+          }
+        }
+      }
+      if (player1 > player2) {
+        console.log('player1 wins!!');
+      } else if (player1 < player2) {
+        console.log('player2 wins!!');
+      } else {
+        console.log('Match NUL!!');
+      }
+    }
   }
 
   getClass(x: number, y: number) {
--- src/app/models/room.ts
+++ src/app/models/room.ts
@@ -3,13 +3,13 @@ import { Player } from './player';
 export class Room {
   players: Player[];
   turn: string;
-  piece: number = 64;
+  piece: number = 60;
   board: { line: number[] }[] = [
     { line: [0, 0, 0, 0, 0, 0, 0, 0] },
     { line: [0, 0, 0, 0, 0, 0, 0, 0] },
     { line: [0, 0, 0, 0, 0, 0, 0, 0] },
-    { line: [0, 0, 0, 0, 0, 0, 0, 0] },
-    { line: [0, 0, 1, 2, 0, 0, 0, 0] },
+    { line: [0, 0, 0, 1, 2, 0, 0, 0] },
+    { line: [0, 0, 0, 2, 1, 0, 0, 0] },
     { line: [0, 0, 0, 0, 0, 0, 0, 0] },
     { line: [0, 0, 0, 0, 0, 0, 0, 0] },
     { line: [0, 0, 0, 0, 0, 0, 0, 0] },
