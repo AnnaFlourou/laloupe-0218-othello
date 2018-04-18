d0d679a5c11ede743d8689509b7835968f0de39a
Jo-zTk <jonathan.zietek@gmail.com>
2018-04-18 18:02:55 +0200

    Fix: Fix travis lint

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -66,7 +66,7 @@ export class BoardComponent implements OnInit, OnDestroy {
                         this.scorePlayer2 += 1;
                       }
                     }
-                    if (this.room.quit === true) {
+                    if (this.room.quit) {
                       this.router.navigate(['game']);
                     }
                   }
